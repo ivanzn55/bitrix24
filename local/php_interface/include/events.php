162c6c449b8a55331c51cacb608ff868e9514eb6
Ivan.Zolotarev <zolotarev.ivan.n@gmail.com>
2026-02-10 11:40:23 +0600

    hw10 Handler events for deal and iblock application

--- local/php_interface/include/events.php
+++ local/php_interface/include/events.php
@@ -1,7 +1,12 @@
 <?php
+
+use Otus\Event\DealHandler;
+use Otus\Event\IblockElementEvent;
+use Otus\Event\IblockElementHandler;
+
 $eventManager = \Bitrix\Main\EventManager::getInstance();
 
-$eventManager->addEventHandlerCompatible('main', 'OnPageStart', '\Otus\Event\Main::handlerPageStart');
+//$eventManager->addEventHandlerCompatible('main', 'OnPageStart', '\Otus\Event\Main::handlerPageStart');
 
 $eventManager->AddEventHandler(
     'iblock',
@@ -9,5 +14,24 @@ $eventManager->AddEventHandler(
     [
         'Otus\UserType\OnlineRecord',
         'GetUserTypeDescription'
+    ]
+);
+
+
+$eventManager->addEventHandler(
+    "iblock",
+    "OnAfterIBlockElementUpdate",
+    [
+        IblockElementHandler::class,
+        'onElementAfterUpdate'
+    ]
+);
+
+$eventManager->addEventHandlerCompatible(
+    "crm",
+    "OnAfterCrmDealUpdate",
+    [
+        DealHandler::class,
+        'onAfterDealUpdate'
     ]
 );
